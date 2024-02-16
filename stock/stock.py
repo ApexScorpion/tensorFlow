7337b1b890a72fa509a08fc5b26db56dee4b8e13
Blake Whitlock <blake_whitlock_2026@outlook.com>
2024-02-15 23:40:00 -0500

    2/15

--- stock/stock.py
+++ stock/stock.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as num
 import pandas as pd
 
-combo = pd.read_csv(https://github.com/ApexScorpion/tensorFlow/blob/main/data/Combined_News_DJIA.csv)
-
-
+combo = pd.read_csv('data\\Combined_News_DJIA.csv')
+data = pd.DataFrame(combo)
+print(data.describe())
+     
